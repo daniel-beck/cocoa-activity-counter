6eb301e812f16002d811e91218f0dc4be78db72a
Daniel Beck <danielbeck@Servus10.local>
2012-07-29 11:48:58 +0200

    Initial Commit

---
+++ CocoaActivityCounter/BWAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BWAppDelegate.m
+//  CocoaActivityCounter
+//
+//  Created by Daniel Beck on 29.07.2012.
+//  Copyright (c) 2012 Daniel Beck. All rights reserved.
+//
+
+#import "BWAppDelegate.h"
+
+@implementation BWAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ CocoaActivityCounter/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CocoaActivityCounter
+//
+//  Created by Daniel Beck on 29.07.2012.
+//  Copyright (c) 2012 Daniel Beck. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
